28d7cc4085f03a5eec0d72fd4cc2ab82459413e0
Jesper Armouti-Hansen <jesper@armoutihansen.xyz>
2020-10-28 16:41:40 +0100

    Several changes

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
+	{" ",	"music",	0,	11},
+	{" ",	"pacpackages",	0,	8},
+	{" ",	"mailbox",	179,	12},
+	{" ",	"news",		0,	6},
+	{"   ",  "kbselect", 	0, 	30},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -14,17 +15,18 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{" ",	"memory",	10,	14},
+	{" ",	"cpu_freq",	10,	18},
+	{" ",	"disk", 	180,	17},
+	/*  {"",	"moonphase",	18000,	17}, */
+	/*  {"",	"nettraf",	1,	16}, */
+	{" ",	"volume",	0,	10},
+	{" ",	"brightness", 	0,	9},
+	{" ",	"battery",	5,	3},
+	{" ",	"internet",	5,	4},
+	{" ",	"weather",	3600,	5},
+	{" ",	"clock",	60,	1},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
